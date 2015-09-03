78b6720a60407156c50b548f2c76cc8519c6f1b3
Kevin Lanni <therealklanni@gmail.com>
2015-09-03 00:33:36 -0700

    get most viewed videos

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import express from 'express'
 const router = express.Router()
 
 import dotty from 'dotty'
+import got from 'got'
 
 router.get('/', function(req, res, next) {
   const viewData = {
@@ -17,7 +18,35 @@ router.get('/', function(req, res, next) {
     viewData.user = user
   }
 
-  res.render('index', viewData);
+  const reqParms = [
+    ['part', 'snippet'],
+    ['channelId', 'UCJ5HW_Wl9NFyZCG_rHozyvw'],
+    ['maxResults', '2'],
+    ['order', 'viewCount'],
+    ['type', 'video'],
+    ['fields', 'items(id%2Csnippet)'],
+    ['key', process.env.YOUTUBE_API_KEY]
+  ]
+
+  got(`https://www.googleapis.com/youtube/v3/search?${reqParms.map(x => x.join('=')).join('&')}`, {
+    json: true
+  })
+    .then(resp => {
+      let items = resp.body.items
+      debug('youtube search results', items)
+      let videos = items.map((video => ({
+        thumbnail: dotty.get(video, 'snippet.thumbnails.high.url'),
+        videoId: dotty.get(video, 'id.videoId'),
+        title: dotty.get(video, 'snippet.title')
+      })))
+      debug(videos)
+      viewData.videos = videos
+      res.render('index', viewData)
+    })
+    .catch(() => {
+      debug('failed to get youtube search results')
+      res.render('index', viewData)
+    })
 });
 
-module.exports = router;
+export default router
